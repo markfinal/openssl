90a81a1cc6493a9ad680a015207b4e6c58d6eded
Mark Final <markfinal@hotmail.com>
2016-02-07 10:44:18 +0000

    [bam] Removed tests from the static library to avoid librarian messages about duplicate main symbols

--- bam/Scripts/OpenSSL.cs
+++ bam/Scripts/OpenSSL.cs
@@ -25,24 +25,24 @@ namespace openssl
             source.AddFiles("$(packagedir)/crypto/aes/*.c");
             source.AddFiles("$(packagedir)/crypto/asn1/*.c");
             source.AddFiles("$(packagedir)/crypto/bio/*.c", filter: new System.Text.RegularExpressions.Regex(@"^((?!.*bss_rtcp).*)$"));
-            source.AddFiles("$(packagedir)/crypto/bn/*.c", filter: new System.Text.RegularExpressions.Regex(@"^((?!.*exp).*)$"));
+            source.AddFiles("$(packagedir)/crypto/bn/*.c", filter: new System.Text.RegularExpressions.Regex(@"^((?!.*exp)(?!.*test\.)(?!.*speed\.).*)$"));
             source.AddFiles("$(packagedir)/crypto/bn/bn_exp.c"); // TODO: not sure how to exclude exp.c from above, but not bn_exp.c - need more RegEx fu
             source.AddFiles("$(packagedir)/crypto/bn/bn_exp2.c"); // TODO: not sure how to exclude exp.c from above, but not bn_exp2.c - need more RegEx fu
             source.AddFiles("$(packagedir)/crypto/buffer/*.c");
             source.AddFiles("$(packagedir)/crypto/cmac/*.c");
             source.AddFiles("$(packagedir)/crypto/cms/*.c");
             source.AddFiles("$(packagedir)/crypto/conf/conf_*.c");
-            source.AddFiles("$(packagedir)/crypto/des/*.c", filter: new System.Text.RegularExpressions.Regex(@"^((?!.*des\.c)(?!.*des_opts)(?!.*read_pwd)(?!.*speed).*)$"));
-            source.AddFiles("$(packagedir)/crypto/dh/*.c");
-            source.AddFiles("$(packagedir)/crypto/dsa/*.c", filter: new System.Text.RegularExpressions.Regex(@"^((?!.*dsagen).*)$"));
+            source.AddFiles("$(packagedir)/crypto/des/*.c", filter: new System.Text.RegularExpressions.Regex(@"^((?!.*des\.c)(?!.*des_opts)(?!.*read_pwd)(?!.*speed)(?!.*test\.)(?!.*rpw).*)$"));
+            source.AddFiles("$(packagedir)/crypto/dh/*.c", filter: new System.Text.RegularExpressions.Regex(@"^((?!.*p512)(?!.*p192)(?!.*p1024)(?!.*dhtest).*)$"));
+            source.AddFiles("$(packagedir)/crypto/dsa/*.c", filter: new System.Text.RegularExpressions.Regex(@"^((?!.*dsagen)(?!.*test\.).*)$"));
             source.AddFiles("$(packagedir)/crypto/dso/*.c");
-            source.AddFiles("$(packagedir)/crypto/ec/*.c", filter: new System.Text.RegularExpressions.Regex(@"^((?!.*ecp_nistz256).*)$"));
-            source.AddFiles("$(packagedir)/crypto/ecdh/*.c");
-            source.AddFiles("$(packagedir)/crypto/ecdsa/*.c");
+            source.AddFiles("$(packagedir)/crypto/ec/*.c", filter: new System.Text.RegularExpressions.Regex(@"^((?!.*ecp_nistz256)(?!.*test\.).*)$"));
+            source.AddFiles("$(packagedir)/crypto/ecdh/*.c", filter: new System.Text.RegularExpressions.Regex(@"^((?!.*test\.).*)$"));
+            source.AddFiles("$(packagedir)/crypto/ecdsa/*.c", filter: new System.Text.RegularExpressions.Regex(@"^((?!.*test\.).*)$"));
             source.AddFiles("$(packagedir)/crypto/engine/*.c", filter: new System.Text.RegularExpressions.Regex(@"^((?!.*test\.).*)$"));
             source.AddFiles("$(packagedir)/crypto/err/*.c");
-            source.AddFiles("$(packagedir)/crypto/evp/*.c", filter: new System.Text.RegularExpressions.Regex(@"^((?!.*e_dsa).*)$")); // TODO: e_dsa not compiling on Windows yet
-            source.AddFiles("$(packagedir)/crypto/hmac/*.c");
+            source.AddFiles("$(packagedir)/crypto/evp/*.c", filter: new System.Text.RegularExpressions.Regex(@"^((?!.*e_dsa)(?!.*test\.).*)$")); // TODO: e_dsa not compiling on Windows yet
+            source.AddFiles("$(packagedir)/crypto/hmac/*.c", filter: new System.Text.RegularExpressions.Regex(@"^((?!.*test\.).*)$"));
             source.AddFiles("$(packagedir)/crypto/lhash/*.c", filter: new System.Text.RegularExpressions.Regex(@"^((?!.*test\.).*)$"));
             source.AddFiles("$(packagedir)/crypto/md5/md5_dgst.c");
             source.AddFiles("$(packagedir)/crypto/md5/md5_one.c");
@@ -58,7 +58,7 @@ namespace openssl
             {
                 source.AddFiles("$(packagedir)/crypto/rand/rand_win.c");
             }
-            source.AddFiles("$(packagedir)/crypto/rsa/*.c");
+            source.AddFiles("$(packagedir)/crypto/rsa/*.c", filter: new System.Text.RegularExpressions.Regex(@"^((?!.*test\.).*)$"));
             source.AddFiles("$(packagedir)/crypto/sha/sha_dgst.c");
             source.AddFiles("$(packagedir)/crypto/sha/sha1dgst.c");
             source.AddFiles("$(packagedir)/crypto/sha/sha1_one.c");
@@ -66,8 +66,8 @@ namespace openssl
             source.AddFiles("$(packagedir)/crypto/sha/sha512.c");
             source.AddFiles("$(packagedir)/crypto/stack/*.c");
             source.AddFiles("$(packagedir)/crypto/ui/*.c");
-            source.AddFiles("$(packagedir)/crypto/x509/*.c");
-            source.AddFiles("$(packagedir)/crypto/x509v3/*.c", filter: new System.Text.RegularExpressions.Regex(@"^((?!.*tabtest\.)(?!.*v3conf)(?!.*v3prin).*)$"));
+            source.AddFiles("$(packagedir)/crypto/x509/*.c", filter: new System.Text.RegularExpressions.Regex(@"^((?!.*test\.).*)$"));
+            source.AddFiles("$(packagedir)/crypto/x509v3/*.c", filter: new System.Text.RegularExpressions.Regex(@"^((?!.*test\.)(?!.*v3conf)(?!.*v3prin).*)$"));
             source.AddFiles("$(packagedir)/engines/*.c", filter: new System.Text.RegularExpressions.Regex(@"^((?!.*gmp).*)$"));
 
             source.PrivatePatch(settings =>
